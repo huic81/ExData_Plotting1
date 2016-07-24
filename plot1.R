cdc2be9644d86a81b6152fd890c50d2305465253
huic <huic81@yahoo.com>
2016-07-24 20:50:15 +0800

    complete assignment

---
+++ plot1.R
@@ -0,0 +1,62 @@
+############################################################################################
+#The overall goal of this project is to examine how household energy usage varies over a 
+#2-day period in February, 2007. 
+#Data Sample:
+#********************************************************************************************
+#Date;Time;Global_active_power;Global_reactive_power;Voltage;Global_intensity;Sub_metering_1;
+#Sub_metering_2;Sub_metering_3
+#21/12/2006;11:22:00;0.244;0.000;242.290;1.000;0.000;0.000;0.000
+#21/12/2006;11:23:00;?;?;?;?;?;?;
+#********************************************************************************************
+#This script is to reconstruct the Global Active Power plot 1 using the base plotting system.
+#Final output from this exercise are:
+#1. plot1.R script that consist of the R codes that produce the plot
+#2. plot1.png which is the plot produced by the code
+#
+#This plot1.R does the following. 
+#1. Download zipped source data & unzipped it
+#2. read the data from just 2 days: 2007-02-01 and 2007-02-02
+#3. convert the Date and Time variables to Date/Time classes in R using the strptime() and 
+#   as.Date() functions
+#4. plot Global Active Power plot 1 and save it to plot1.png file with a width of 480 pixels 
+#   and a height of 480 pixels
+############################################################################################
+
+############################################################################################
+#1.Preparation,  Get & Clean Data
+############################################################################################
+## step 1: load required packages
+library(data.table)
+library(lubridate)
+
+## step 2: Set Working Directory to folder where plot1.R was stored
+setwd("C:/Users/hwee.see.teh/Documents/Git/ExData_Plotting1")
+
+## step 3: download zip file from website
+## make data folder if not exists
+if(!file.exists("./data")) {dir.create("./data")}
+## download the zip file
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destfilepath <- "./data/household_power_consumption.zip"
+download.file(fileUrl,destfile = destfilepath)
+
+## step 4: unzip data
+zipfile <- unzip(destfilepath, exdir = "./data")
+
+## step 5: Load Data
+dataColClass <- c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric')
+powerconsumption <- read.table("./data/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = dataColClass)
+
+## step 6: read the data from just 2 days: 2007-02-01 and 2007-02-02
+powerconsumption <- powerconsumption[powerconsumption$Date=='1/2/2007' | powerconsumption$Date=='2/2/2007',]
+
+## step 7: convert the Date and Time variables to Date/Time classes in R using the strptime() and 
+## as.Date() functions 
+powerconsumption$DateTime <- strptime(paste(powerconsumption$Date, powerconsumption$Time), format="%d/%m/%Y %H:%M:%S")
+
+############################################################################################
+#2.plot Global Active Power plot 1 (plot1.png - 480h x 480w)
+############################################################################################
+png('plot1.png',width=480,height=480)
+hist(powerconsumption$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+dev.off()
